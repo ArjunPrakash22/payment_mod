6f3191f8569df94f1d96a2c6e0681cadd2a1eab4
ummalfajeela <ummalfajeela25@gmail.com>
2024-09-03 10:47:17 +0530

    faji

--- payment_mod/frontend/src/Pages/Dashboard/Dashboard.jsx
+++ payment_mod/frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 const Dashboard = () => {
   const [student, setStudent] = useState({});
   const location = useLocation();
-  const { key } = location.state || {}; // Extract 'key' from location.state
+  const { key } = location.state || {}; 
 
   useEffect(() => {
     async function fetchData() {
@@ -37,27 +37,31 @@ const Dashboard = () => {
         <img className="clg-logo" src={clglogo} alt="clg-logo" />
       </div>
       <h2>Student Dashboard</h2>
-      <div className="student-info">
+      <div className="student-info"><br/>
+      <h1 style={{ color: '#17a462', display: 'block', marginBottom: '20px' }}>Personal Details</h1><br/>
+
         <p><strong>Admission No:</strong> {student.admission_no}</p>
         <p><strong>Registration No:</strong> {student.regno}</p>
         <p><strong>Name:</strong> {student.name}</p>
         <p><strong>Gender:</strong> {student.gender}</p>
-        <p><strong>Date of Birth:</strong> {student.dob}</p>
         <p><strong>Email:</strong> {student.email}</p>
         <p><strong>Phone No:</strong> {student.phone_no}</p>
         <p><strong>Aadhar No:</strong> {student.aadhar_no}</p>
-        <p><strong>Govt School:</strong> {student.govt_school}</p>
-        <p><strong>Course Name:</strong> {student.course_name}</p>
+        <p><strong>DOB:</strong> {student.dob}</p>
         <p><strong>Batch Year:</strong> {student.batchyr}</p>
-        <p><strong>Quota:</strong> {student.quota}</p>
+        <br/>
+        <div><h1 style={{ color: '#17a462', display: 'block', marginBottom: '20px' }}>College Details</h1><br/></div><br/>
+
+        <p><strong>Course Name:</strong> {student.course_name}</p>
+      
         <p><strong>College Fees:</strong> {student.clg_fees}</p>
         <p><strong>Hosteller:</strong> {student.hosteller}</p>
         <p><strong>Hostel Fees:</strong> {student.hostel_fees}</p>
         <p><strong>Tuition Fees:</strong> {student.tuition_fees}</p>
-        <p><strong>Miscellaneous Fees:</strong> {student.miscellaneous_fees}</p>
-        <p><strong>Reason:</strong> {student.reason}</p>
         <p><strong>Transport Fees:</strong> {student.transport_fees}</p>
         <p><strong>Exam Fees:</strong> {student.exam_fees}</p>
+        <p><strong>Miscellaneous Fees:</strong> {student.miscellaneous_fees}</p>
+        <p><strong>Reason:</strong> {student.reason}</p>
       </div>
     </div>
   );
